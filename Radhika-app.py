0f0d566473b35e931478c75288343f1a48f185bb
spsachin87 <66319593+spsachin87@users.noreply.github.com>
2021-06-15 16:09:25 +0530

    Update Radhika-app.py

--- Radhika-app.py
+++ Radhika-app.py
@@ -42,4 +42,3 @@ dispatcher.add_handler(CommandHandler('start',start))
 dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command),input_message))
 updater.start_polling()
 updater.idle()   
-   
